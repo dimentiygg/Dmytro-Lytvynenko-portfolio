1b0592268f4203be7a5a8e2030bc4335feb95db2
dimentiygg <145362704+dimentiygg@users.noreply.github.com>
2025-11-19 12:46:21 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,12 +53,12 @@ const Hero = () => {
         <div className="embla__container relative flex gap-5 max-sm:gap-2 ">
           {slides.map(({ name, img, link, containerColor, index }) => (
             <a
-              className="last:mr-5 max-sm:last:mr-2"
+              className=" max-w-[800px] last:mr-5 max-sm:last:mr-2"
               target="blank"
               href={link}
             >
               <div
-                className="embla__slide flex justify-center items-center h-[60vh] p-[40px] group border-containerBorder border rounded-lg max-sm:px-[18px] max-sm:py-0 "
+                className="embla__slide flex justify-center items-center max-h-[60vh] p-[40px] group border-containerBorder border rounded-lg max-sm:px-[18px] max-sm:py-0 "
                 key={index}
                 style={{ backgroundColor: containerColor }}
               >
