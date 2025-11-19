2736be8ddc5699bf945e9b1a5b383adcaaa8cbb4
dimentiygg <145362704+dimentiygg@users.noreply.github.com>
2025-11-19 12:18:17 +0200

    Update LinksMenu.jsx

--- src/components/LinksMenu.jsx
+++ src/components/LinksMenu.jsx
@@ -7,7 +7,11 @@ const LinksMenu = ({ linkRef = null }) => {
     <ul className=" flex flex-col gap-2 pointer-events-auto font-helvetica font-normal text-base text-blckMain ">
       <li>
         <AnimateIcon animateOnHover>
-          <a className="flex flex-row gap-2" target="_blank" href="">
+          <a
+            className="flex flex-row gap-2"
+            target="_blank"
+            href="https://www.linkedin.com/in/dima-lytvynenko/"
+          >
             <div className=" relative z-200 flex items-center justify-center w-5 h-5 border-[0.5px] border-blckMain rounded-xs bg-white">
               <SquareArrowOutUpRight />
             </div>
@@ -19,7 +23,11 @@ const LinksMenu = ({ linkRef = null }) => {
       </li>
       <li>
         <AnimateIcon animateOnHover>
-          <a className="flex flex-row gap-2" target="_blank" href="">
+          <a
+            className="flex flex-row gap-2"
+            target="_blank"
+            href="https://github.com/dimentiygg"
+          >
             <div className="flex items-center justify-center w-5 h-5 border-[0.5px] border-blckMain rounded-xs bg-white">
               <SquareArrowOutUpRight />
             </div>
